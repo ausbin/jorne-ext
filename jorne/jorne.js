33a157e154e22c55520fdbccf285c8a10b341203
Austin Adams <git@austinjadams.com>
2017-01-22 11:09:14 -0500

    Handle non-existent logos gracefully
    When a page has no element with the .mw-wiki-logo class, don't throw an
    error.

--- jorne/jorne.js
+++ jorne/jorne.js
@@ -1,1 +1,4 @@
-document.getElementsByClassName("mw-wiki-logo")[0].style.backgroundImage = "url('https://i.imgur.com/s9gqfK2.png')";
+var logo = document.getElementsByClassName("mw-wiki-logo")[0];
+
+if (logo !== undefined)
+    logo.style.backgroundImage = "url('https://i.imgur.com/s9gqfK2.png')";
